a6f5c45aabab464678d067f0e6e70269224e9021
andersongabsilva <Andersongabriel_maria@hotmail.com>
2020-03-08 23:45:19 -0300

    Tratando Exceçoes. Com blocos "try" e "catch"

---
+++ TestaDivisao-LidandocomERROS_3/ContaCorrente.cs
@@ -0,0 +1,97 @@
+﻿// using _05_ByteBank;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+        public static double TaxaDeOperacao { get; set; }
+       
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        //Construtor da Conta Corrente
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+
+            //Quanto mais contas, ou seja quanto mais for incrementando o Total de Contas Criadas
+            //Menor será a taxa desta conta corrente
+            TaxaDeOperacao = 30 / TotalDeContasCriadas;
+
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
